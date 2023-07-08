66231820e97a0262ab2233c269c9d465c2144304
qmafog <qmafog6@gmail.com>
2023-07-08 16:42:55 +0300

    done

--- FilterByPalindromic/ArrayExtension.cs
+++ FilterByPalindromic/ArrayExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+#pragma warning disable
 
 namespace FilterByPalindromicTask
 {
@@ -18,9 +19,44 @@ namespace FilterByPalindromicTask
         /// {12345, 1111111112, 987654, 56, 1111111, -1111, 1, 1233321, 70, 15, 123454321}  => { 1111111, 123321, 123454321 }
         /// {56, -1111111112, 987654, 56, 890, -1111, 543, 1233}  => {  }.
         /// </example>
-        public static int[] FilterByPalindromic(int[]? source)
+        public static bool IsPalindrome(int number)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string str = number.ToString();
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        public static int[] FilterByPalindromic(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Empty", nameof(array));
+            }
+            List<int> result = new List<int>();
+
+            foreach (int number in array)
+            {
+                if (IsPalindrome(number))
+                    result.Add(number);
+            }
+
+            return result.ToArray();
         }
     }
 }
